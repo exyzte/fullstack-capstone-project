426191c210462551dea3c441da65282896bcff81
exyzte <davidecicerys@gmail.com>
2026-01-22 10:22:27 -0500

    completed navbar element

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -9,7 +9,9 @@ export default function Navbar() {
                 <ul className="navbar-nav">
                     {/* Task 1: Add links to Home and Gifts below*/}
                     <li className="nav-item">
-                        <a className="nav-link" href="/home">Home</a>
+                        <a className="nav-link" href="/home.html">Home</a>
+                        <a className="nav-link" href="/app">Gifts</a>
+
                     </li>
                 </ul>
             </div>
