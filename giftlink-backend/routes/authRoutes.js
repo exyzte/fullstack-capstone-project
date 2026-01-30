4f4812171c30cea4142fa115b1edf70d25794df1
exyzte <davidecicerys@gmail.com>
2026-01-30 17:56:17 -0500

    removed unnecessary imports

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const app = express();
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { body, validationResult } = require('express-validator');
